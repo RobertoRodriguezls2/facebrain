a7a9900ce33ca70bdf19fa13b17272734392222c
RobertoRodriguezls2 <robertrodriguezls26.0@gmail.com>
2022-01-21 20:20:43 -0800

    Added face recognition with clarifai

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
+import FaceRecognition from './Components/FaceRecognittion/FaceRecognition';
 import './App.css';
 import Particles from "react-tsparticles";
+import Clarifai from 'clarifai';
 
 
 const particlesOptions = {
@@ -29,98 +31,153 @@ const particlesLoaded = (container) => {
   console.log(container);
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles
-      className='particles'
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        
-        fpsLimit: 60,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: "repulse",
-            },
-            onHover: {
-              enable: false,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: false,
-            speed: 6,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: true,
-      }}
-    />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
+const app = new Clarifai.App({
+  apiKey: "f5537c6a21fc4914bc3fbbd8d8729c0a",
+});
 
-      {/* 
+
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: [],
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+   const calrifyFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+   const image = document.getElementById('inputimage');
+   const width = Number(image.width);
+   const height = Number(image.height);
+   return {
+     leftCol : calrifyFace.left_col * width,
+     topRow : calrifyFace.top_row * height,
+     rightCol: width - (calrifyFace.right_col * width),
+     bottomRow: height - (calrifyFace.bottom_row * height)
+   }
+
+   
+
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then( response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
       
-      <FaceRecognition /> */}
-    </div>
-  );
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles
+          className='particles'
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+
+            fpsLimit: 60,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: false,
+                  mode: "repulse",
+                },
+                onHover: {
+                  enable: false,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                bubble: {
+                  distance: 400,
+                  duration: 2,
+                  opacity: 0.8,
+                  size: 40,
+                },
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outMode: "bounce",
+                random: false,
+                speed: 2,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: true,
+                value: 5,
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit} />
+      
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/> 
+      </div>
+    );
+  }
+
 }
 
 export default App;
